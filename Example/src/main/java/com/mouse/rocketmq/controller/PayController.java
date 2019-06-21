ab216675c390591b275aedddd92906dc71aa44cc
shilun <sl166199@163.com>
2019-06-21 13:57:01 +0800

    测试调整

--- Example/src/main/java/com/mouse/rocketmq/controller/PayController.java
+++ Example/src/main/java/com/mouse/rocketmq/controller/PayController.java
@@ -4,15 +4,18 @@ import com.mouse.rocketmq.jms.PayProducer;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author shil <sl166199@163.com>
@@ -36,10 +39,18 @@ public class PayController {
 
         Message message = new Message(topic,"taga", ("hello rocketmq = "+text).getBytes() );
 
-        SendResult sendResult = defaultMQProducer.send(message);
+        message.setDelayTimeLevel(1);//延时发送
+
+        SendResult sendResult = defaultMQProducer.send(message, (List<MessageQueue> mqs, Message msg, Object arg) -> {
+            Integer queueNum = (Integer)arg;
+            return mqs.get(queueNum);
+        },0);
+
+//        defaultMQProducer
 
         System.out.println(sendResult);
 
         return new HashMap<>();
+
     }
 }
